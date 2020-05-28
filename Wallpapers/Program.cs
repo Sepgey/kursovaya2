ebe3a75a1fbfdbdb60f155b161089cf46e6816be
starberdyev <starberdyev@mail.ru>
2020-05-28 22:25:00 +0300

    add project

---
+++ Wallpapers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wallpapers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
