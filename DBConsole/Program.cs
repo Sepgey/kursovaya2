6244d393b7151cec21468048c486f26cc628895e
starberdyev <starberdyev@mail.ru>
2020-05-29 22:06:06 +0300

    Каркас-начало

---
+++ DBConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
