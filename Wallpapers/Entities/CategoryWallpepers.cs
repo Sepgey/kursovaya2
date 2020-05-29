87dcb86ab5699af0b1b44fe756ed48bfcce335cd
Falkonny <62031372+Falkonny@users.noreply.github.com>
2020-05-29 13:31:21 +0300

    class Category added

---
+++ Wallpapers/Entities/CategoryWallpepers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+
+namespace Wallpapers.Entities
+{
+	public class CategoryWallpepers
+	{
+		public int id { get; set; }
+		public string categiryName { get; set; }
+		public string description { get; set; }
+	}
+}
