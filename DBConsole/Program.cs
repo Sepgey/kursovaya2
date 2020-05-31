6f60a94af7178354a999f13961e3b0b7df8abc00
Falkonny <62031372+Falkonny@users.noreply.github.com>
2020-05-31 18:51:42 +0300

    DBConsole added, some methods fixed

--- DBConsole/Program.cs
+++ DBConsole/Program.cs
@@ -1,10 +1,41 @@
-﻿using Microsoft.EntityFrameworkCore.Design;
+﻿using Wallpapers.Entities;
+using Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using System;
+using Microsoft.Extensions.Options;
+using EO.Internal;
 
 namespace DBConsole
 {
     public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext> 
     {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        OptionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WallpaperDb;Trusted_Connection=True;", b => b.MigrationsAssembly("Infrastructure"));
+            return new AppDbContext(optionsBuilder.Options);
+    }
 
+    class Program
+    {
+        private static readonly AppDbContext _appContext;
+        private static IWallpaperRepository _wallpaperRepository;
+        private static IUserRepository _authorRepository;
+        static Program()
+        {
+            AppDbContextFactory factory = new AppDbContextFactory();
+            _appContext = factory.CreateDbContext(null);
+            _authorRepository = new UserRepository(_appContext);
+            _wallpaperRepository = new WallpaperRepository(_appContext);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start!");
+
+            User author = new User("Александр", "Пушкин", "Сергеевич");
+            _authorRepository.Add(author);
+            Wallpaper wallpaper= new Wallpaper();
+            _wallpaperRepository.Add(wallpaper);
+        }
     }
 }
--- Wallpapers/Entities/AuditableEntity.cs
+++ Wallpapers/Entities/AuditableEntity.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Wallpapers.Entities
 {
-    public abstract class AuditableEntity
+    public abstract class AuditableEntity : Entity
     {
         public DateTime CreatedAt { get; set; }
 
